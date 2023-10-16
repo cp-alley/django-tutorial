f894df43597cebd6ac1397e4f361718f39a40c7e
Jung-GunSong <jsong781@gmail.com>
2023-10-16 19:30:14 -0400

    working on testing for question model

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+
+from .models import Question
+
+
+def create_question(question_text, days):
+    """
+    creates a question for testing with question content and days from today
+    when question was made
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        """ tests that if no questions exists, right message appears"""
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response, "No polls are available")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+
+class QuestionModelTests(TestCase):
+    """tests our question model"""
+    def test_was_published_recently_with_future_question(self):
+        """Test if a question published many days in the future is not considered recent"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        Test if a question created one sec more than a day from today
+        is not considered recent
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+        Test if a question created one sec less than a day from today
+        is considered recent
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
